f4f9ad255c8176af1b350e683fdce8004a5b0019
Duraimurugan-H <hduraimurugan@gmail.com>
2024-09-14 23:49:18 +0530

    cors url added

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "https://login-frontend-iota.vercel.app", credentials: true }));
+// Array of allowed origins
+const allowedOrigins = [
+  "https://login-frontend-iota.vercel.app", 
+  "http://localhost:5173" // Add your additional URL here
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    // Check if the origin is in the allowed origins list or if there's no origin (for server-to-server requests)
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
@@ -22,14 +38,14 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+  connectDB();
+  console.log("Server is running on port: ", PORT);
 });
