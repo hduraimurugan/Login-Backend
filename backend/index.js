953920d1b58b167b87f676766bd3a5c9cd86ff96
Duraimurugan-H <hduraimurugan@gmail.com>
2024-09-14 06:03:45 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-app.use(cors({ origin: "https://login-frontend-iota.vercel.app/", credentials: true }));
+app.options("*", cors({ origin: "https://login-frontend-iota.vercel.app", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
 	connectDB();
